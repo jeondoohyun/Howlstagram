fa4eba1763b09c95c5073f0bfb4fb45de3ebff7a
dhjeon <sonlcr1@naver.com>
2023-06-02 17:04:04 +0900

    알람 리스트 페이지 만들기 ing..

--- app/src/main/java/com/example/howlstagram/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/howlstagram/navigation/AlarmFragment.kt
@@ -4,16 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.howlstagram.R
+import com.example.howlstagram.databinding.FragmentAlarmBinding
+import com.example.howlstagram.databinding.FragmentGridBinding
+import com.example.howlstagram.navigation.model.AlarmDTO
 
 class AlarmFragment : Fragment{
     // 좋아요 관련 정보창 4번째 바텀 네비게이션
     constructor() : super()
 
+    private var mBinding: FragmentAlarmBinding? = null
+    private val binding get() = mBinding!!
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
-        return view
+        mBinding = FragmentAlarmBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var alarmDTOList : ArrayList<AlarmDTO> = arrayListOf()
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+
+            return CustomViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewholder = (holder as CustomViewHolder)
+
+            when (alarmDTOList[position].kind) {
+                0 -> {
+                    val str_0 = alarmDTOList[position].userId + getString(R.string.alarm_favorite)
+                    viewholder.commentviewitem_textview_profile.text = str_0
+                }
+                1 -> {  // todo :
+                    val str_0 = alarmDTOList[position].userId + getString(R.string.alarm_favorite)
+                    viewholder.commentviewitem_textview_profile.text = str_0
+                }
+                2 -> {
+                    val str_0 = alarmDTOList[position].userId + getString(R.string.alarm_favorite)
+                    viewholder.commentviewitem_textview_profile.text = str_0
+                }
+            }
+
+        }
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
+
+        inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+            val commentviewitem_imageview_profile = view.findViewById<ImageView>(R.id.commentviewitem_imageview_profile)
+            val commentviewitem_textview_profile = view.findViewById<TextView>(R.id.commentviewitem_textview_profile)
+            val commentviewitem_textview_comment = view.findViewById<TextView>(R.id.commentviewitem_textview_comment)
+        }
+
     }
 }
--- app/src/main/java/com/example/howlstagram/navigation/model/AlarmDTO.kt
+++ app/src/main/java/com/example/howlstagram/navigation/model/AlarmDTO.kt
@@ -4,7 +4,7 @@ data class AlarmDTO(
     var destinationUid : String? = null,
     var userId : String? = null,        // 이메일
     var uid : String? = null,
-    var kind : Int? = null,     // 어떤 타입의 메세지 종류인지 알수 있는 변수(좋아요인지, 댓글인지 등?)
+    var kind : Int? = null,     // 0 : like alarm, 1 : comment alarm, 2 : follow alarm
     var message : String? = null,
     var timeStamp : Long? = null
 
